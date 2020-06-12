2293cd39360a0217962fd3e956a2fa0b760d5715
Pozadr <adpozor@gmail.com>
2020-06-12 19:14:16 +0200

    Main task 3/6

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,13 +1,31 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many ml of water the coffee machine has: ");
+        int waterMagazine = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has: ");
+        int milkMagazine = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
+        int coffeeBeansMagazine = scanner.nextInt();
+        System.out.println("Write how many cups of coffee you will need: ");
+        int userOrder = scanner.nextInt();
+
+        // How many cups is possible to make?
+        int howManyCupsWater = waterMagazine / 200;
+        int howManyCupsMilk= milkMagazine / 50;
+        int howManyCupsCoffeeBeans = coffeeBeansMagazine / 15;
+        int howManyCupsPossible = Math.min(Math.min(howManyCupsWater, howManyCupsMilk), howManyCupsCoffeeBeans);
+        if (howManyCupsPossible == userOrder) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (howManyCupsPossible > userOrder) {
+            System.out.println("Yes, I can make that amount of coffee (and even " + (howManyCupsPossible - userOrder) + " more than that)");
+        } else {
+            System.out.println("No, I can make only " + howManyCupsPossible + " cup(s) of coffee");
+        }
+
     }
 }
