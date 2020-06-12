9e9acc9ddd294fb60f9f6f596b91aad5d6fd3854
Pozadr <adpozor@gmail.com>
2020-06-12 22:43:53 +0200

    Main rask 5/6 done

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -4,28 +4,126 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
+        CoffeeMachine coffeeMachine = new CoffeeMachine(400, 540, 120, 9, 550);
+        coffeeMachine.menu();
+    }
+
+    private int waterMagazine;
+    private int milkMagazine;
+    private int coffeeBeansMagazine;
+    private int disposableCups;
+    private int money;
+
+    public CoffeeMachine (int waterMagazine, int milkMagazine, int coffeeBeansMagazine, int disposableCups, int money) {
+        this.waterMagazine = waterMagazine;
+        this.milkMagazine = milkMagazine;
+        this.coffeeBeansMagazine = coffeeBeansMagazine;
+        this.disposableCups = disposableCups;
+        this.money = money;
+    }
+
+    public void displayCoffeeMachineStatus() {
+        System.out.println("The coffee machine has:");
+        System.out.println(waterMagazine + " of water");
+        System.out.println(milkMagazine + " of milk");
+        System.out.println(coffeeBeansMagazine + " of coffee beans");
+        System.out.println(disposableCups + " of disposable cups");
+        System.out.println(money + " of money");
+    }
+
+    public void menu() {
+        boolean exit = true;
+        Scanner scanner = new Scanner(System.in);
+        while(exit) {
+            System.out.println("Write action (buy, fill, take, remaining, exit): ");
+            String userDecision = scanner.next();
+            switch (userDecision) {
+                case "buy": {
+                    System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, " +
+                            "back - to main menu: ");
+                    String userDecisionBuyCoffee = scanner.next();
+                    switch (userDecisionBuyCoffee) {
+                        case "1": {
+                            buyCoffee(250,0, 16, 4);
+                            break;
+                        }
+                        case "2": {
+                            buyCoffee(350,75, 20, 7);
+                            break;
+                        }
+                        case "3": {
+                            buyCoffee(200,100, 12, 6);
+                            break;
+                        }
+                        case "back": {
+                            break;
+                        }
+                        default: {
+                            System.out.println("Wrong input.");
+                        }
+                    }
+                    break;
+                }
+                case "fill": {
+                    fillCoffeeMachine();
+                    break;
+                }
+                case "take": {
+                    takeCoffeMachineMoney();
+                    break;
+                }
+                case "remaining": {
+                    displayCoffeeMachineStatus();
+                    break;
+                }
+                case "exit": {
+                    exit = false;
+                    break;
+                }
+                default: {
+                    System.out.println("Wrong input.");
+                }
+            }
+        }
+    }
+
+    public void buyCoffee(int water, int milk, int coffeeBeans, int cost) {
+        if (disposableCups > 0 && this.waterMagazine >= water && this.milkMagazine >= milk
+                && this. coffeeBeansMagazine >= coffeeBeans) {
+            System.out.println("I have enough resources, making you a coffee!");
+            this.waterMagazine -= water;
+            this.milkMagazine -= milk;
+            this.coffeeBeansMagazine -= coffeeBeans;
+            this.money += cost;
+            this.disposableCups -= 1;
+        } else if (this.waterMagazine < water) {
+            System.out.println("Sorry, not enough water!");
+        } else if (this.milkMagazine < milk) {
+            System.out.println("Sorry, not enough milk!");
+        } else if (this.coffeeBeansMagazine < coffeeBeans) {
+            System.out.println("Sorry, not enough coffee beans!");
+        } else {
+            System.out.println("Error!");
+        }
+    }
+
+    public void fillCoffeeMachine() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Write how many ml of water the coffee machine has: ");
-        int waterMagazine = scanner.nextInt();
+        this.waterMagazine += scanner.nextInt();
         System.out.println("Write how many ml of milk the coffee machine has: ");
-        int milkMagazine = scanner.nextInt();
+        this.milkMagazine += scanner.nextInt();
         System.out.println("Write how many grams of coffee beans the coffee machine has: ");
-        int coffeeBeansMagazine = scanner.nextInt();
-        System.out.println("Write how many cups of coffee you will need: ");
-        int userOrder = scanner.nextInt();
-
-        // How many cups is possible to make?
-        int howManyCupsWater = waterMagazine / 200;
-        int howManyCupsMilk= milkMagazine / 50;
-        int howManyCupsCoffeeBeans = coffeeBeansMagazine / 15;
-        int howManyCupsPossible = Math.min(Math.min(howManyCupsWater, howManyCupsMilk), howManyCupsCoffeeBeans);
-        if (howManyCupsPossible == userOrder) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if (howManyCupsPossible > userOrder) {
-            System.out.println("Yes, I can make that amount of coffee (and even " + (howManyCupsPossible - userOrder) + " more than that)");
-        } else {
-            System.out.println("No, I can make only " + howManyCupsPossible + " cup(s) of coffee");
-        }
+        this.coffeeBeansMagazine += scanner.nextInt();
+        System.out.println("Write how many disposable cups of coffee do you want to add: ");
+        this.disposableCups += scanner.nextInt();
 
     }
+
+    public void takeCoffeMachineMoney() {
+        System.out.println("I gave you $" + this.money);
+        this.money = 0;
+    }
+
+
 }
